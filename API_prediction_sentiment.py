7a698f24f4f6202a8499b0deae5d4c92293bfcdd
Saoussen Attia <attia.saoussen@gmail.com>
2025-01-24 13:21:12 +0100

    Modification API

--- API_prediction_sentiment.py
+++ API_prediction_sentiment.py
@@ -2,29 +2,23 @@
 # coding: utf-8
 
 import os
+import re
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, validator
 import joblib
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-import re
 from pathlib import Path
 
 # Configuration des chemins pour les ressources NLTK
-NLTK_DATA_PATH = (
-    str(Path.home() / "nltk_data") if "CI" in os.environ
-    else os.path.join(
-        r'C:\Users\attia\P7_Réalisez_une_analyse_de_sentiments_grâce_au_Deep_Learning\environnement\myenv', 
-        'nltk_data'
-    )
-)
+NLTK_DATA_PATH = str(Path.home() / "nltk_data")  # Utilisation du dossier "nltk_data" dans le répertoire utilisateur
 
 # Créer le dossier pour les données NLTK s'il n'existe pas
 os.makedirs(NLTK_DATA_PATH, exist_ok=True)
 nltk.data.path.append(NLTK_DATA_PATH)
 
-# Télécharger les ressources NLTK nécessaires
+# Télécharger les ressources NLTK nécessaires si elles sont manquantes
 RESOURCES = ['wordnet', 'omw-1.4', 'stopwords', 'punkt']
 for resource in RESOURCES:
     try:
@@ -43,14 +37,14 @@ except Exception as e:
 # Fonction pour nettoyer le texte
 def clean_text(text: str) -> str:
     """
-    Nettoie le texte en supprimant ponctuation, chiffres et en convertissant en minuscule.
+    Nettoie le texte en supprimant la ponctuation, les chiffres et en convertissant tout en minuscule.
     """
     text = text.lower()
     text = re.sub(r'[^\w\s]', '', text)  # Supprime la ponctuation
     text = re.sub(r'\d+', '', text)  # Supprime les chiffres
     return text
 
-# Fonction pour prétraiter le texte
+# Fonction pour prétraiter le texte : nettoyage, tokenisation, lemmatisation
 def preprocess_text(text: str) -> str:
     """
     Prétraite le texte : nettoyage, tokenisation, lemmatisation.
@@ -70,7 +64,7 @@ async def read_root():
 
 # Chargement du modèle
 try:
-    pipeline = joblib.load('best_model.pkl')
+    pipeline = joblib.load('best_model.pkl')  # Charger le modèle enregistré
 except Exception as e:
     raise RuntimeError(f"Échec du chargement du pipeline : {str(e)}")
 
@@ -109,6 +103,7 @@ async def predict(input_data: InputData):
         print(f"Erreur lors de la prédiction : {str(e)}")
         raise HTTPException(status_code=500, detail=f"Erreur lors de la prédiction : {str(e)}")
 
+# Code pour démarrer l'API
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8080)
--- run_api.py
+++ run_api.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
 import subprocess
 import time
 
 # Lancer l'API FastAPI en arrière-plan
 subprocess.Popen(["uvicorn", "API_prediction_sentiment:app", "--host", "127.0.0.1", "--port", "8080", "--log-level", "debug"])
 
-# Attendez un peu pour vous assurer que l'API a démarré
+# Attendre un peu pour vous assurer que l'API a démarré
 time.sleep(5)  # attendre 5 secondes pour donner à l'API le temps de démarrer
 
 
