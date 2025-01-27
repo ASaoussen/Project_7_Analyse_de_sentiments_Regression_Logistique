c99aa416de4cfdf8e83d7bd1d6a75f9ea68bf92d
Saoussen Attia <attia.saoussen@gmail.com>
2025-01-27 21:29:47 +0100

    Modification app

--- run_api.py
+++ run_api.py
@@ -5,7 +5,7 @@ import subprocess
 import time
 
 # Lancer l'API FastAPI en arrière-plan
-subprocess.Popen(["uvicorn", "API_prediction_sentiment:app", "--host", "127.0.0.1", "--port", "8080", "--log-level", "debug"])
+subprocess.Popen(["uvicorn", "app:app", "--host", "127.0.0.1", "--port", "8080", "--log-level", "debug"])
 
 # Attendre un peu pour vous assurer que l'API a démarré
 time.sleep(5)  # attendre 5 secondes pour donner à l'API le temps de démarrer
